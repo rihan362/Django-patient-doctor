9be3ddf83b2f911e31921944f9bb3f30f6124f00
rihan <saifirihan26@gmail.com>
2024-07-01 18:03:14 +0530

    third commit

--- Userauthintern/settings.py
+++ Userauthintern/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%j9l1ih96^s)qkb1sd0(zgw@p@q(@*1ky$vt^47w*r_ha!%+gg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
